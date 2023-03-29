de95a92c0e847223c19a846cfde87556b718a09a
Andrei Sheleg <andrej.sheleg@gmail.com>
2023-03-29 13:59:08 +0300

    добавлен метод map, чтобы приложение не падало при входном массиве task в неидеальных условиях

--- src/App.tsx
+++ src/App.tsx
@@ -10,17 +10,20 @@ export type TaskType = {
 
 function App() {
 
-    const todoListTitle_1: string = "What to learn"
-    const todoListTitle_2: string = "What to buy"
+    const todoListTitle_1: string = 'What to learn'
+    const todoListTitle_2: string = 'What to buy'
+
     const tasks_1: Array<TaskType> = [
-        {id: 1, title: "HTML & CSS", isDone: true},
-        {id: 2, title: "JS", isDone: true},
-        {id: 3, title: "React", isDone: false},
+        {id: 1, title: "HTML & CSS", isDone: true}, //0
+        {id: 2, title: "JS", isDone: true}, //1
+        {id: 3, title: "React", isDone: false}, //2
     ]
     const tasks_2: Array<TaskType> = [
         {id: 4, title: "Beer", isDone: false},
         {id: 5, title: "Water", isDone: true},
         {id: 6, title: "Cola", isDone: true},
+        {id: 7, title: "Kefirrrr", isDone: true},
+        {id: 8, title: "KMilkMilkMilk", isDone: false},
     ]
 
 
--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,29 +1,39 @@
 import React from 'react';
-import {TaskType} from "./App";
+import {TaskType} from './App';
 
 type TodoListPropsType = {
     title: string
-    tasks: Array<TaskType>
+    tasks: TaskType[]
 }
 export const TodoList = (props: TodoListPropsType) => {
+// в продакшене не делают логигу или методы мэп внутри вёрстки,
+    // мэп выносят или в переменную или в компоненту
+    const tasksMap =  props.tasks.map((el) => {
+        debugger
+        return (
+            <li>
+                <input type="checkbox" checked={el.isDone}/>
+                <span>{el.title}</span>
+            </li>
+        )
+    })
+
     return (
         <div>
             <h3>{props.title}</h3>
             <div>
-                <input placeholder='write new task...'/>
+                <input placeholder="write new task..."/>
                 <button>+add</button>
             </div>
-            <ul>
-                <li><input type="checkbox" checked={props.tasks[0].isDone}/>
-                    <span>{props.tasks[0].title}</span>
-                </li>
-                <li><input type="checkbox" checked={props.tasks[1].isDone}/>
-                    <span>{props.tasks[1].title}</span>
-                </li>
-                <li><input type="checkbox" checked={props.tasks[2].isDone}/>
-                    <span>{props.tasks[2].title}</span>
-                </li>
 
+            <ul>
+                {tasksMap}
+
+
+                {/*<li><input type="checkbox" checked={props.tasks[0].isDone}/><span>{props.tasks[0].title}</span></li>
+                <li><input type="checkbox" checked={props.tasks[1].isDone}/><span>{props.tasks[1].title}</span></li>
+                <li><input type="checkbox" checked={props.tasks[2].isDone}/><span>{props.tasks[2].title}</span></li>
+                <li><input type="checkbox" checked={props.tasks[3].isDone}/><span>{props.tasks[3].title}</span></li>*/}
             </ul>
             <div>
                 <button>All</button>
